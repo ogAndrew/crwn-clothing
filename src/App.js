7f86ca11a50ddc243f45076790efe35630779f30
Andrew Talle <talle.andrew@gmail.com>
2021-01-19 15:32:19 -0800

    update handle submit for sign-in

--- src/App.js
+++ src/App.js
@@ -20,22 +20,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapshot => {
+        userRef.onSnapshot((snapshot) => {
           this.setState({
             currentUser: {
               id: snapshot.id,
-              ...snapshot.data()
-            }
+              ...snapshot.data(),
+            },
           });
-        })
+        });
       }
 
       this.setState({ currentUser: userAuth });
-    })
+    });
   }
 
   componentWillUnmount() {
